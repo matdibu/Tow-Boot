c524d3197a23fca3ad44dda4a2ccd3e8bc62a455
Matei Dibu <contact@mateidibu.dev>
2024-07-31 23:19:22 +0300

    rock64: init

--- modules/tow-boot/config.nix
+++ modules/tow-boot/config.nix
@@ -84,7 +84,7 @@ in
       # -----
 
       # Ensures white text on black background
-      SYS_WHITE_ON_BLACK = yes;
+      SYS_WHITE_ON_BLACK = if withLogo then yes else no;
 
       # Ensures we're not using Truetype
       CONSOLE_TRUETYPE = no;
