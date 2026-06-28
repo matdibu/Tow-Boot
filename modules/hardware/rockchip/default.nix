520c4c3a8244ab35fc5885885e8abbd9fa803b1e
BeatLink <git@beatlink.simplelogin.com>
2026-06-28 02:06:08 -0500

    remove redundant config

--- modules/hardware/rockchip/default.nix
+++ modules/hardware/rockchip/default.nix
@@ -176,10 +176,6 @@ in
                             BOOTSTD_FULL = yes;
                             BOOTSTD_DEFAULTS = lib.mkForce yes;
                             DISTRO_DEFAULTS = lib.mkForce no;
-                            BOOTMETH_EXTLINUX = yes; # extlinux.conf (most ARM distros)
-                            BOOTMETH_EFILOADER = yes; # EFI applications
-                            BOOTMETH_EFI_BOOTMGR = yes; # EFI boot manager variables
-                            BOOTMETH_SCRIPT = yes; # boot.scr scripts
                             BOOTCOMMAND = lib.mkForce (freeform ''"bootflow scan"'');
                             USE_BOOTCOMMAND = yes;
 
